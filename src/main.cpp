4579d656d27d4b1c07158a41802bdebb694cc440
JRazek <jakub.jakubecky123@gmail.com>
2022-04-28 18:49:17 +0200

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <bitmap.hpp>
+
+auto main()->int{
+    using namespace jr::img;
+    std::cout<<"Hello :)\n";
+    
+    Image img(10, 10, 3);
+
+    auto it=img.begin();
+
+}
