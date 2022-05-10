7f3bf79c275b08ebfb13e4583f6cec420c68ce27
JRazek <jakub.jakubecky123@gmail.com>
2022-05-11 00:49:48 +0200

    todo meson

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,10 @@
 #include <bitmap.hpp>
 #include <image_formatters.hpp>
 #include <fstream>
+#include <cassert>
 
 auto main()->int{
     using namespace jr::img;
-    
     Bitmap img(10, 10, 3);
     auto file=std::fstream("/home/user/CLionProjects/imageSteganography/test_resources/image.ppm");
 
