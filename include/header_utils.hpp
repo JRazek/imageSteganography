36c5dc6e49620345a612bba3c41f673f291e8157
user <jakub.razek2@gmail.com>
2022-06-26 16:48:23 +0200

    ppm size fix

--- include/header_utils.hpp
+++ include/header_utils.hpp
@@ -3,11 +3,11 @@
 #include <algorithm>
 #include <cstdint>
 #include <cstdio>
+#include <iostream>
 #include <memory>
 #include <stdexcept>
 #include <string>
 #include <vector>
-#include <iostream>
 
 #include "utils.hpp"
 
@@ -53,16 +53,18 @@ struct BMPHeader : Header {
 
     file_size =
         bytes_to_little_endianness<std::uint32_t>(header.begin() + 0x2, 4);
+
     first_data_byte =
         bytes_to_little_endianness<std::uint32_t>(header.begin() + 0xA, 4);
 
     reserved1 =
         bytes_to_little_endianness<std::uint16_t>(header.begin() + 0x6, 8);
+
     reserved2 =
         bytes_to_little_endianness<std::uint16_t>(header.begin() + 0x8, 10);
 
-    image_size =
-        bytes_to_little_endianness<std::uint32_t>(header.begin() + 0x22, 4);
+    image_size = bytes_to_little_endianness<std::uint32_t>(
+        header.begin() + 0x22, 4);  // TODO fix - invalid size
   }
 
   auto get_image_size() -> std::size_t override { return image_size; }
@@ -94,43 +96,41 @@ struct PPMHeader : Header {
 
     std::size_t line_num = 0;
 
-	std::vector<std::uint8_t> space_separated_nums;
+    std::vector<std::uint8_t> space_separated_nums;
 
-	bool is_comment=false;
+    bool is_comment = false;
     while (it != header.end() && line_num < 3) {
-
       if (*it == '\n') {
-		++it;
-        if (it != header.end() && *it != '#'){
+        ++it;
+        if (it != header.end() && *it != '#') {
           ++line_num;  // this will ignore all lines starting with #
-		  is_comment=false;
-		}
-      }else{
-		  if(*it == '#'){ is_comment=true; }
-		  else if(!is_comment && line_num>0){
-			space_separated_nums.push_back(*it);
-		  }
-		  ++it;
-	  }
-	
+          is_comment = false;
+        }
+      } else {
+        if (*it == '#') {
+          is_comment = true;
+        } else if (!is_comment && line_num > 0) {
+          space_separated_nums.push_back(*it);
+        }
+        ++it;
+      }
     }
 
-	image_size=1;
+    image_size = 1;
 
-	space_separated_nums.push_back(' ');
+    space_separated_nums.push_back(' ');
 
-	std::uint32_t num=0;
+    std::uint32_t num = 0;
 
-	for(auto ch : space_separated_nums){
-		if(ch!=' '){
-			num *= 10;
-			num += ch-'0';
-		}
-		else{
-			image_size *= num;
-			num=0;
-		}
-	}
+    for (auto ch : space_separated_nums) {
+      if (ch != ' ') {
+        num *= 10;
+        num += ch - '0';
+      } else {
+        image_size *= num;
+        num = 0;
+      }
+    }
 
     if (line_num != 3) throw JRException("ppm P6 corrupted!");
 
